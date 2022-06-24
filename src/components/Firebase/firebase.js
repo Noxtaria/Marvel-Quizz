ec5bca400cc092a7971c9d2fcc12df2ae4a538c4
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-06-24 17:48:57 +0200

    Authentification via Firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDd1yRG59q3o_2sXiMlHDZP_mHml2Af6n0",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
