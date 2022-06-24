c9343988282d1bc0df1ff13600b5e2fd05f9af2a
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-06-24 16:59:08 +0200

    Installation firebase + Configuration

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDd1yRG59q3o_2sXiMlHDZP_mHml2Af6n0",
+    authDomain: "marvel-quizz-6615f.firebaseapp.com",
+    projectId: "marvel-quizz-6615f",
+    storageBucket: "marvel-quizz-6615f.appspot.com",
+    messagingSenderId: "376152000350",
+    appId: "1:376152000350:web:3086577856d006e23271cf"
+  };
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig)
+    }
+}
+
+export default Firebase;
